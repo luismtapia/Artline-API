6ffe5dafa86b20f09774a8d9b72e871aba68baba
14030598 <14030598@itcelaya.edu.mx>
2021-09-13 22:35:22 -0500

    instalacion de mongoose

--- models/Publicacion.js
+++ models/Publicacion.js
@@ -1,13 +1,16 @@
 // Clase para las publicaciones, 
-class Publicacion{
-    constructor(id, CREATE_AT, idUsuario, imagen, comentario, likes){
-        this.id = id;
-        this.CREATE_AT = CREATE_AT; //fecha de creacion de la publicación: default new Date()
-        this.idUsuario = idUsuario;
-        this.imagen = imagen;
-        this.comentario = comentario; //pensado en principio como un arreglo o objeto para evitar crear otra entidad
-        this.likes = likes;
-    }
-}
+// class Publicacion{
+//     constructor(id, CREATE_AT, idUsuario, imagen, comentario, likes){
+//         this.id = id;
+//         this.CREATE_AT = CREATE_AT; //fecha de creacion de la publicación: default new Date()
+//         this.idUsuario = idUsuario;
+//         this.imagen = imagen;
+//         this.comentario = comentario; //pensado en principio como un arreglo o objeto para evitar crear otra entidad
+//         this.likes = likes;
+//     }
+// }
 
-module.exports = Publicacion;
+// module.exports = Publicacion;
+
+
+const mongoose = require('mongoose');
